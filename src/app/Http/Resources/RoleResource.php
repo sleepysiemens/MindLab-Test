f99baa67f239df81781099be722cf74a0d1e24f4
siemens <2001sema@gmail.com>
2026-02-17 00:15:28 +0700

    RoleResource fix

--- src/app/Http/Resources/RoleResource.php
+++ src/app/Http/Resources/RoleResource.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 /**
+ * @property int $id
  * @property string $name
  * @property string $guard_name
  */
@@ -15,6 +16,7 @@ class RoleResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id'         => $this->id,
             'name'       => $this->name,
             'guard_name' => $this->guard_name,
         ];
